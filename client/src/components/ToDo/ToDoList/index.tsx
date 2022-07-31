76dec3938cc719bfbc9bab4abad2dc99f28d0d26
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-07-31 19:56:19 +0900

    feat: getAll 키값 변경 및 경고 메세지

--- client/src/components/ToDo/ToDoList/index.tsx
+++ client/src/components/ToDo/ToDoList/index.tsx
@@ -2,13 +2,24 @@ import ToDoAPI from "@/api/toDo";
 import { useAuthStore } from "@/store/useAuthStore";
 import { Box, Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
 import FloatingButton from "../../FloatingButton";
 import ToDoModalForm from "../FormModal";
 
 const ToDoList = () => {
   const authToken = useAuthStore((state) => state.token);
-  const { data, isLoading } = useQuery(["toDoList"], () =>
-    ToDoAPI.getAll(authToken)
+  const { data, isLoading } = useQuery(
+    ["toDoList", authToken],
+    () => ToDoAPI.getAll(authToken),
+    {
+      onError: (error) => {
+        toast.error("로그인하고 이용해 주세요.", {
+          autoClose: 3000,
+          position: toast.POSITION.TOP_RIGHT,
+          draggable: true,
+        });
+      },
+    }
   );
   console.log(data);
 
